c2dbe7ef03d694bbbe54bc0dc02c78da90d5fd1a
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2022-12-16 14:08:56 +0100

    v2

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const myKey = config.APIkey;
-const request = new XMLHttpRequest();
-request.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=51.4988&lon=3.6110&units=metric&exclude=minutely,daily,alerts&appid="+myKey, true); // MIDDELBURG
-//request.open("GET", "https://jsonplaceholder.typicode.com/users") // FROM TUTORIAL
-request.send();
-request.onload = () => {
-    console.log(request)
-};
+
+document.getElementById('button1').addEventListener('click', loadMiddelburg);
+
+
+// MIDDELBURG
+function loadMiddelburg(){ 
+    const requestMb = new XMLHttpRequest();
+    requestMb.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=51.4988&lon=3.6110&units=metric&exclude=minutely,daily,alerts&appid="+myKey, true); 
+
+    requestMb.onload = function() {
+        if(this.status == 200){
+            let weather = JSON.parse(this.responseText);
+            console.log(weather);
+        }
+        else if (this.status == 404) {
+            console.log('error 404');
+        }
+    }
+    requestMb.onerror = function () {
+        console.log('request error');
+    }
+
+    requestMb.send();
+}
