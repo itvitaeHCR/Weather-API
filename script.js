3d0e3af078061fc0fd7acc6f426e53b769f68305
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2022-12-21 12:51:46 +0100

    v6

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function getLocation(){
         case 'Middelburg':
             loadMiddelburg();
             break;
+        case 'Vlissingen':
+            loadVlissingen();
+            break;
         case '0':
             break;
     }
@@ -31,7 +34,41 @@ function loadMiddelburg(){
             const forecast = 
 
             `
-            <div id="city">
+            <div id="city" style="background-color:#59a2deb0">
+            <h2>The Weather in ${weather.name} right now: </h2>
+            <p id="temp"> The current temperature is ${weather.main.temp} celsius</p>
+            <p id="temp"> But it rather feels like ${weather.main.feels_like} celsius</p>
+            </div>
+            `;
+
+            document.getElementById('forecast').innerHTML = forecast;
+        }
+        else if (this.status == 404) {
+            console.log('error 404');
+        }
+    }
+    requestMb.onerror = function () {
+        console.log('request error');
+    }
+
+    requestMb.send();
+}
+
+// MIDDELBURG
+function loadVlissingen(){ 
+    const requestMb = new XMLHttpRequest();
+    requestMb.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=51.4537&lon=3.5709&units=metric&exclude=minutely,daily,alerts&appid="+myKey, true); 
+
+    requestMb.onload = function() {
+        if(this.status == 200){
+            let weather = JSON.parse(this.responseText);
+            console.log(weather);
+
+            // WEATHER FORECAST FUNCTIONS HERE:
+            const forecast = 
+
+            `
+            <div id="city" style="background-color:#59a2deb0">
             <h2>The Weather in ${weather.name} right now: </h2>
             <p id="temp"> The current temperature is ${weather.main.temp} celsius</p>
             <p id="temp"> But it rather feels like ${weather.main.feels_like} celsius</p>
